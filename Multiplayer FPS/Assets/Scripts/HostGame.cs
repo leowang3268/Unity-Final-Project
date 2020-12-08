ab2fcd7aafb8fe9edf3f556321f90cd409259726
ChrisDeantana <christiandeantanastudy@gmail.com>
2020-12-08 10:07:13 +0800

    Lobby

---
+++ Multiplayer FPS/Assets/Scripts/HostGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+public class HostGame : MonoBehaviour {
+
+    [SerializeField]
+    private uint roomSize = 6;
+
+    private string roomName;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void CreateRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating Room: " + roomName + " with room for " + roomSize + " Players.");
+            networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
